34970cc02d226bd615dcc2d9b5d807b0de728323
Jay Lee <busyjaylee@gmail.com>
2018-03-23 21:12:04 +0800

    test cache

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #![crate_type = "lib"]
 #![cfg_attr(test, feature(test))]
 #![feature(fnbox)]
